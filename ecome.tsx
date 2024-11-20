3b0e5a18e45491ebe435c9218d066dad7b2b1880
lubnakhan-Github-Account <lk4231455@gmail.com>
2024-11-20 19:56:12 +0500

    first commit

---
+++ ecome.tsx
@@ -0,0 +1,39 @@
+
+
+export default function Ecom(){
+
+return(
+    <><div className="bg-blue-300">
+        <div className="bg-pink-200 rounded-br-full rounded-tl-full ">
+        <nav className="flex justify-evenly h-[120px]">
+            <button className="hover:text-red-500 font-bold">Covid 19</button>
+            <button className="hover:text-red-500 font-bold">Symptomes</button>
+            <button className="hover:text-red-500 font-bold">Diagnosis</button>
+            <button className="hover:text-red-500 font-bold">Contact</button>
+        </nav>
+    </div><div className="flex justify-around ">
+            <div className="flex flex-col mt-11 w-[300px]">
+                <div className="mt-11 h-64 bg-center bg-cover bg-[url('https://th.bing.com/th/id/OIP.HcK3Kv8mJSjrYxXm_Xo01AHaEo?rs=1&pid=ImgDetMain')] text-white">covid19</div>
+                <div className="mt-11 h-64 bg-center bg-cover bg-[url('https://www.emeraldgrouppublishing.com/sites/default/files/image/covid-cells.jpg')] text-white">varient</div>
+            </div>
+            <div className="w-[200px] mt-11  ">
+                <h2 className=" text-4xl mt-11 ">Stay Safe Stay Home </h2>
+                <p className="mt-9">Keep you and your surronding clean.
+                    Avoid sneeeze and spite here and there.it cause desiese.
+                    And spreed covid 19 and its varients quickly.
+                    To prevent it cover your mouth when you sneeze.
+                    Wear mask before outside the home.
+                </p>
+            </div>
+
+        </div><footer className="bg-pink-300 flex justify-center px-5 py-5 font-bold">
+            Copy Rights 2024 & Devolope By Uzma Khan
+        </footer>
+    </div>
+    </>
+                )
+            }
+                    
+
+         
+     
